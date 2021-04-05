a2dc662e6a8aab8d6e98e672b6ff4f51d1ca527a
Faisal-msh <faisal.msh25@gmail.com>
2021-04-05 19:25:28 +0300

    Make basics setting of my project

--- website/website/settings.py
+++ website/website/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'web.apps.WebConfig',
+    'use.apps.UseConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
